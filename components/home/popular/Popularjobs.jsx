c32cd655034ee27eb90d4d4279b2c97f1c01a224
amulyamarali <97819763+amulyamarali@users.noreply.github.com>
2023-04-10 10:03:13 +0530

    first commit

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { 
+  View, Text,TouchableOpacity, FlatList, ActivityIndicator
+} from 'react-native';
+
+import { useRouter } from 'expo-router';
+
+import styles from './popularjobs.style';
+
+import { COLORS, SIZES } from '../../../constants' ;
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+
+import useFetch from '../../../hook/useFetch';
+
+const Popularjobs = () => {
+  const router = useRouter();
+  // const isLoading = false;
+  // const error = false;
+
+  const {data, isLoading, error } = useFetch
+  ('search',{
+    query: 'React developer',
+    num_pages: 1
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data} // put what u fetch from api here 
+            renderItem={({ item }) => (
+              <PopularJobCard 
+              item={item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs
---
+++ components/jobdetails/company/Company.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text, Image } from 'react-native'
+
+import styles from './company.style'
+
+import { icons } from '../../../constants'
+import { checkImageURL } from '../../../utils'
+
+const Company = ({companyLogo, jobTitle, companyName, location}) => {
+  return (
+    <View>
+      <Text>Company</Text>
+    </View>
+  )
+}
+
+export default Company
